89b182f5dc8eac9259b921c3fdb8ce9ce333fb17
AshwiniMadapura <ashwinidivakar8@gmail.com>
2022-03-31 16:41:11 +0100

    functions updated

--- demo.js
+++ demo.js
@@ -5,7 +5,70 @@ const buttons=document.querySelectorAll(".buttonDesign")
             
 
     })
- 
+    // /const equalfunc=()=>{
+    const equalfunc=()=>{
+        if(addFlag==true){
+            equalFlag=true;
+            addtwoNum()
+        }
+        if(subFlag==true){
+            equalFlag=true;
+            subtwoNum()
+        }
+        document.getElementById("result").value=document.getElementById("result").value
+        console.log(document.getElementById("result").value)
+    }
+    function clickNumber(){
+        document.getElementById("result").value+=this.value;
+    }
+    
+    function addtwoNum()
+    {
+        if(equalFlag==false){
+            addFlag=true
+            document.getElementById("result").value+= this.value
+        }   
+        else if(equalFlag==true){
+            let sumTemp=0;
+            for(let i=0;i<document.getElementById("result").value.length;i++){
+                const parsval=parseInt(document.getElementById("result").value[i])
+                if(isNaN(parsval)){    
+                }
+                else{
+                    sumTemp+= parseInt(document.getElementById("result").value[i])   
+                }
+            }
+            document.getElementById("result").value=sumTemp;
+    
+        }
+    }
+    
+    function subtwoNum()
+    {
+        if(equalFlag==false){
+            subFlag=true
+            document.getElementById("result").value+= this.value
+        }   
+        else if(equalFlag==true){
+            let sumTemp=0;
+            for(let i=0;i<document.getElementById("result").value.length;i++){
+                console.log(i)
+                const parsval=parseInt(document.getElementById("result").value[i])
+                if(isNaN(parsval)){    
+                }
+                else{
+                    if(sumTemp==0){
+                        sumTemp=parseInt(document.getElementById("result").value[i]);   
+                    }
+                    else{
+                    sumTemp -= parseInt(document.getElementById("result").value[i])   
+                    console.log(sumTemp);
+                }
+                }
+            }
+            document.getElementById("result").value=sumTemp;
+    }
+    }
 
 // document.getElementById("1").addEventListener("click", clickNumber);
 // document.getElementById("2").addEventListener("click",clickNumber);
@@ -26,52 +89,4 @@ let subFlag=false;
 let equalFlag=false;
 
 // const equalfunc=()=>{
-function equalfunc(){
-    if(addFlag==true){
-        equalFlag=true;
-        addtwoNum()
-    }
-    if(subFlag==true){
-        subtwoNum()
-    }
-    document.getElementById("result").value=document.getElementById("result").value
-    console.log(document.getElementById("result").value)
-}
-function clickNumber(){
-    document.getElementById("result").value+=this.value;
-}
 
-function addtwoNum()
-{
-    if(equalFlag==false){
-        addFlag=true
-        document.getElementById("result").value+= this.value
-    }
-    
-
-    else if(equalFlag==true){
-        let sumTemp=0;
-        for(let i=0;i<document.getElementById("result").value.length;i++){
-            const parsval=parseInt(document.getElementById("result").value[i])
-            if(isNaN(parsval)){    
-            }
-            else{
-                sumTemp+= parseInt(document.getElementById("result").value[i])   
-            }
-        }
-        document.getElementById("result").value=sumTemp;
-
-    }
-}
-
-function subtwoNum()
-{
-    if(document.getElementById("result").value[1]==null){
-        subFlag=true
-        document.getElementById("result").value+= this.value
-    }
-    else if(document.getElementById("result").value[0] !=null && document.getElementById("result").value[2]!=null ){
-        document.getElementById("result").value=parseInt(document.getElementById("result").value[0])-parseInt(document.getElementById("result").value[2])
-        console.log(document.getElementById("result").value) 
-    }
-}

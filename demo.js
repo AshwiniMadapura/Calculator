cd603b8d5e01e0b5966ff81ede880e0b9bbf9ebf
AshwiniMadapura <ashwinidivakar8@gmail.com>
2022-03-31 16:17:43 +0100

    n numbers can be added

--- demo.js
+++ demo.js
@@ -1,19 +1,34 @@
-const buttons=document.querySelectorAll("button")
-    
+const buttons=document.querySelectorAll(".buttonDesign")
         buttons.forEach(item=>{
+            console.log(typeof(item.type));
             item.addEventListener("click",clickNumber);
+            
 
     })
  
+
+// document.getElementById("1").addEventListener("click", clickNumber);
+// document.getElementById("2").addEventListener("click",clickNumber);
+// document.getElementById("3").addEventListener("click",clickNumber);
+// document.getElementById("4").addEventListener("click",clickNumber);
+// document.getElementById("5").addEventListener("click",clickNumber);
+// document.getElementById("6").addEventListener("click",clickNumber);
+// document.getElementById("7").addEventListener("click",clickNumber);
+// document.getElementById("8").addEventListener("click",clickNumber);
+// document.getElementById("9").addEventListener("click",clickNumber);
+// document.getElementById("0").addEventListener("click",clickNumber);
 document.getElementById("plus").addEventListener("click",addtwoNum);
 document.getElementById("subtract").addEventListener("click",subtwoNum);
 
 document.getElementById("equal").addEventListener("click",equalfunc);
 let addFlag=false;
 let subFlag=false;
+let equalFlag=false;
 
-const equalfunc=()=>{
+// const equalfunc=()=>{
+function equalfunc(){
     if(addFlag==true){
+        equalFlag=true;
         addtwoNum()
     }
     if(subFlag==true){
@@ -28,14 +43,23 @@ function clickNumber(){
 
 function addtwoNum()
 {
-    if(document.getElementById("result").value[1]==null){
+    if(equalFlag==false){
         addFlag=true
         document.getElementById("result").value+= this.value
     }
-    else if(document.getElementById("result").value[0] !=null && document.getElementById("result").value[2]!=null ){
-    document.getElementById("result").value=parseInt(document.getElementById("result").value[0])+parseInt(document.getElementById("result").value[2])
-    console.log(document.getElementById("result").value) 
-    // parseInt(document.getElementById("3").value[1])+parseInt(document.getElementById("3").value[0])
+    
+
+    else if(equalFlag==true){
+        let sumTemp=0;
+        for(let i=0;i<document.getElementById("result").value.length;i++){
+            const parsval=parseInt(document.getElementById("result").value[i])
+            if(isNaN(parsval)){    
+            }
+            else{
+                sumTemp+= parseInt(document.getElementById("result").value[i])   
+            }
+        }
+        document.getElementById("result").value=sumTemp;
 
     }
 }

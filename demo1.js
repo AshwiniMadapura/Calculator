0a08a078be4ffe8f6ed2dddeb33ddfcb131002bf
AshwiniMadapura <ashwinidivakar8@gmail.com>
2022-04-01 10:33:53 +0100

    updated comments

--- demo1.js
+++ demo1.js
@@ -19,21 +19,31 @@ let flag1=true
 function clickNumber(){
     if(flag1==true){
         document.getElementById("result").value+=this.value;
-        if(num_array.length==0){
-            num_array[0]=this.value;
-        }
+        if(num_array.length==0){ 
+                num_array[0]=this.value;
+            }
         else{
-        let newVal=num_array[num_array.length-1]+this.value;
-        num_array[num_array.length-1]=parseInt( newVal);
-        }
+            if(this.value == "."){
+                let newVal=num_array[num_array.length-1]+this.value;
+                num_array[num_array.length-1]=newVal;       
+            }
+            else if(this.value == "+-"){
+                let newVal=0 - num_array[num_array.length-1];
+                console.log(newVal)
+                console.log(typeof(newVal))
+                num_array[num_array.length-1]=newVal;       
+                }
+            else{
+                let newVal=num_array[num_array.length-1]+this.value;
+                num_array[num_array.length-1]=Number(newVal);
+                }
+            }
     }
     else if(flag1==false){
         document.getElementById("result").value+=this.value;
-        
-        num_array.push(parseInt(this.value))
+        num_array.push(Number(this.value))
         flag1=true;
     }
-
     
 }
 
@@ -46,7 +56,7 @@ function clickOperations(){
 
 function evalFunc(){
     let totalValue = num_array[0];
-
+    console.log(num_array)
     for(i=0;i<num_array.length-1;i++){
         if (oper_array[i] == "+"){
             totalValue = totalValue + num_array[i+1];
@@ -60,6 +70,10 @@ function evalFunc(){
         else if(oper_array[i]=="/"){
             totalValue = totalValue / num_array[i+1];
         }
+    }
+
+    if(oper_array[i]=="%"){
+        totalValue = totalValue / 100;
     }
     document.getElementById("result").value = totalValue;
 }
